f83c5b8073712a230567a039076d83ff63221e25
ikunxdlyj <yjlei@stu.xidian.edu.cn>
2025-08-18 14:16:41 +0800

    Initial commit

--- DynamicPlanning/MaximumAmount.java
+++ DynamicPlanning/MaximumAmount.java
@@ -1,36 +1,36 @@
 package DynamicPlanning;
 
 public class MaximumAmount {
-    public int maximumAmount(int[][] coins) {
-
-    }
-    public int f(int i,int j,int chance,int[][] coins) {
+//    public int maximumAmount(int[][] coins) {
+//
+//    }
+//    public int f(int i,int j,int chance,int[][] coins) {
+////        if(i==coins.length-1&&j==coins[0].length-1){
+////            if(coins[i][j]>0){
+////                return coins[i][j];
+////            }else {
+////                if(chance>0){
+////                    return 0;
+////                }else{
+////                    return coins[i][j];
+////                }
+////            }
+////        }
 //        if(i==coins.length-1&&j==coins[0].length-1){
-//            if(coins[i][j]>0){
-//                return coins[i][j];
-//            }else {
-//                if(chance>0){
-//                    return 0;
-//                }else{
-//                    return coins[i][j];
-//                }
-//            }
+//            return coins[i][j]>0?coins[i][j]:chance>0?0:coins[i][j];
 //        }
-        if(i==coins.length-1&&j==coins[0].length-1){
-            return coins[i][j]>0?coins[i][j]:chance>0?0:coins[i][j];
-        }
-        if(i==coins.length-1){
-//            if(coins[i][j]>0){
-//                return coins[i][j]+f(i,j+1,chance,coins);
-//            }else{
-//                if(chance>0){
-//                    return f(i,j+1,chance-1,coins);
-//                }else{
-//                    return coins[i][j]+f(i,j+1,chance,coins);
-//                }
-//            }
-
-            return coins[i][j]>0?(coins[i][j]+f(i,j+1,chance,coins)):chance>0?f(i,j+1,chance-1,coins) : f(i,j+1,chance,coins) ;
-        }
-    }
+//        if(i==coins.length-1){
+////            if(coins[i][j]>0){
+////                return coins[i][j]+f(i,j+1,chance,coins);
+////            }else{
+////                if(chance>0){
+////                    return f(i,j+1,chance-1,coins);
+////                }else{
+////                    return coins[i][j]+f(i,j+1,chance,coins);
+////                }
+////            }
+//
+//            return coins[i][j]>0?(coins[i][j]+f(i,j+1,chance,coins)):chance>0?f(i,j+1,chance-1,coins) : f(i,j+1,chance,coins) ;
+//        }
+//    }
 }
