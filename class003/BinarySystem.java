a1558d9c860a44167f977e2582a49ee2b0eab21d
yjlei <yjlei@stu.xidian.edu.cn>
2025-08-23 17:26:33 +0800

    8/23

--- class003/BinarySystem.java
+++ class003/BinarySystem.java
@@ -2,12 +2,22 @@ package class003;
 
 public class BinarySystem {
     public static void main(String[] args) {
-        printBinary(32);
+        for(int i=0;i<=100;i++){
+            int num=(int)(Math.random()*10000);
+            printBinary(num);
+            printBinary(rightOne(num));
+            System.out.println("---------------------------");
+        }
     }
+
+    public static int rightOne(int num) {
+        return num & (-num);
+    }
+
     public static void printBinary(int num) {
         StringBuilder sb = new StringBuilder();
-        for(int i=31; i>=0; i--) {
-            sb.append((num>>i)&1);
+        for (int i = 31; i >= 0; i--) {
+            sb.append((num >> i) & 1);
         }
         System.out.println(sb);
     }
